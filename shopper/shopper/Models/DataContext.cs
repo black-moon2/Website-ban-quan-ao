a88e40b72031b2cd37e80441448489e19ca2e346
black-moon2 <nguyentranvinh25042003@gmail.com>
2023-10-24 09:52:45 +0700

    thêm

---
+++ shopper/shopper/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using shopper.Models;
+
+namespace shopper.Models
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options) : base(options)
+		{
+		}
+		public DbSet<Menu> Menus { get; set; }
+	}
+}
